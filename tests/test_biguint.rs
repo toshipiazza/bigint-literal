8732ecf4e24237a5d615c52b9dc28d8b8643b66f
Toshi Piazza <toshi.piazza@gmail.com>
2024-06-10 11:53:56 -0700

    Adds another test

--- tests/test_biguint.rs
+++ tests/test_biguint.rs
@@ -9,6 +9,18 @@ fn test_base10_implicit_radix() {
 }
 
 #[test]
+fn test_base10_implicit_radix_large() {
+    // test large bignum that will overflow several u32 limbs
+    let b = biguint!("340282366920938463463374607431768211455"); // 2^128-1 = 0xffffffffffffffffffffffffffffffff
+    let digs = b.to_u32_digits();
+    assert_eq!(digs.len(), 4); // b should have 4 u32 limbs
+    assert_eq!(digs[0], 0xffffffffu32);
+    assert_eq!(digs[1], 0xffffffffu32);
+    assert_eq!(digs[2], 0xffffffffu32);
+    assert_eq!(digs[3], 0xffffffffu32);
+}
+
+#[test]
 fn test_base10_explicit_radix() {
     let a: BigUint = 10000000u32.into();
     let b = biguint!("10000000", 10);
