c02cf5706e126544cdbf85731d0e40c1ec2ecd3d
Toshi Piazza <toshi.piazza@gmail.com>
2024-06-10 11:45:18 -0700

    Restructures tests

--- tests/test_biguint.rs
+++ tests/test_biguint.rs
@@ -1,68 +1,79 @@
 use bigint_literal::biguint;
-use num::{BigUint, FromPrimitive};
+use num::BigUint;
 
 #[test]
 fn test_base10_implicit_radix() {
+    let a: BigUint = 10000000u32.into();
     let b = biguint!("10000000");
-    assert_eq!(b, BigUint::from_u32(10000000).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base10_explicit_radix() {
+    let a: BigUint = 10000000u32.into();
     let b = biguint!("10000000", 10);
-    assert_eq!(b, BigUint::from_u32(10000000).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base16_explicit_radix() {
+    let a: BigUint = 0xabcdefu32.into();
     let b = biguint!("abcdef", 16);
-    assert_eq!(b, BigUint::from_u32(0xabcdef).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base10_integer_literal_u8() {
+    let a: BigUint = 250u8.into();
     let b = biguint!(250u8);
-    assert_eq!(b, BigUint::from_u8(250).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base10_integer_literal_u16() {
+    let a: BigUint = 65530u16.into();
     let b = biguint!(65530u16);
-    assert_eq!(b, BigUint::from_u16(65530).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base10_integer_literal_u32() {
+    let a: BigUint = 3405691582u32.into();
     let b = biguint!(3405691582u32);
-    assert_eq!(b, BigUint::from_u32(3405691582u32).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base10_integer_literal_u64() {
+    let a: BigUint = 18446744073709551615u64.into();
     let b = biguint!(18446744073709551615u64);
-    assert_eq!(b, BigUint::from_u64(18446744073709551615u64).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base16_integer_literal_u8() {
+    let a: BigUint = 0xffu8.into();
     let b = biguint!(0xffu8);
-    assert_eq!(b, BigUint::from_u8(0xff).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base16_integer_literal_u16() {
+    let a: BigUint = 0xffffu16.into();
     let b = biguint!(0xffffu16);
-    assert_eq!(b, BigUint::from_u16(0xffff).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base16_integer_literal_u32() {
+    let a: BigUint = 0xffffffffu32.into();
     let b = biguint!(0xffffffffu32);
-    assert_eq!(b, BigUint::from_u32(0xffffffff).unwrap());
+    assert_eq!(a, b);
 }
 
 #[test]
 fn test_base16_integer_literal_u64() {
+    let a: BigUint = 0xffffffffffffffffu64.into();
     let b = biguint!(0xffffffffffffffffu64);
-    assert_eq!(b, BigUint::from_u64(0xffffffffffffffff).unwrap());
+    assert_eq!(a, b);
 }
